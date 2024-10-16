537d5e5e75e5d0bdb7810d2b0ad5ccfb176c4cbb
Eric-Coutinho <Eric.Coutinhop@hotmail.com>
2024-10-16 09:16:24 -0300

    saving mouse movement and clicks, keyboard inputs and creating checkpoints

--- inputs.py
+++ inputs.py
@@ -2,47 +2,91 @@ from pynput import mouse, keyboard
 
 isRunning = False
 
+movements = []
+checkpoints = []
+
 def running():
     global isRunning
-    if not isRunning:
-        return False
+    if isRunning:
+        return True
+    return False
+
+def playMacro(movements):
+    for movement in movements:
+        print(movement)
 
 def on_press(key):
+    global isRunning
+    global movements
+    global checkpoints
+
     running()
     print('Tecla pressionada: {0}'.format(
         key
     ))
 
-def on_release(key):
-    running()
-    global isRunning
-    print('Released {0}'.format(
-        key
-    )) 
-    
     if key == keyboard.Key.esc:
+        checkpoint = {"checkpoint": movements.copy()}
+        checkpoints.append(checkpoint)
+        movements.clear()
         isRunning = False
         print("Parando listeners")
+        print("checkpoints: ", checkpoints)
+        playMacro(movements = checkpoints)
         return False
 
+    if key == keyboard.Key.right:
+        print("movimentos: ", movements)
+        checkpoint = {"checkpoint": movements.copy()}
+        checkpoints.append(checkpoint)
+        movements.clear()
+
+def on_release(key):
+    running()
+    global movements
+    
+    print('Released {0}'.format(
+        key
+    ))
+
+    if key != keyboard.Key.right:
+        movement = {"keyboard": key}
+        movements.append(movement)
+ 
 def on_move(x,  y):
+    global movements
+
     if running() is False:
         return False
     print('Mouse movido para {0}'.format((x, y)))
+    movement  = {"mouse_position": (x, y)}
+    movements.append(movement)
+    
 
 def on_click(x, y, button, pressed):
+    global movements
+
     if running() is False:
         return False
     print('{0} at {1}'.format(
         'Pressed' if pressed else 'Released', (x, y)
     ))
+    if pressed:
+        movement = {"mouse_clicked": (x, y)}
+    else:
+        movement = {"mouse_released": (x, y)}
+    movements.append(movement)
 
 def on_scroll(x, y, dx, dy):
+    global movements
+
     if running() is False:
         return False
     print('Scrolled at {0} to {1}'.format(
         'down' if dy < 0 else 'up', (x, y)
     ))
+    movement = {"mouse_scroll": ((x, y), (dx, dy))}
+    movements.append(movement)
 
 def start_listeners():
     global isRunning
