38fb593a877a3ceb5380abffbfe36b0f6375ec2b
Eric-Coutinho <Eric.Coutinhop@hotmail.com>
2024-10-16 13:22:22 -0300

    reproducing cursor path AND clicks

--- macro.py
+++ macro.py
@@ -1,6 +1,7 @@
 import time
 from pynput import mouse, keyboard
-from pynput.mouse import Controller, Button
+from pynput.mouse import Controller
+from pynput.keyboard import Controller
 import pyautogui
 
 isRunning = False
@@ -8,11 +9,9 @@ isRunning = False
 movements = []
 checkpoints = []
 mouse_controller = Controller()
+keyboard_controller = Controller()
 last_time = time.time()
-click_time = time.time()
-release_time = time.time()
-
-last_click_time = time.time()
+last_press = time.time()
 
 screen_width, screen_height = pyautogui.size()
 print(f'Resolução da tela: {screen_width}x{screen_height}')
@@ -24,41 +23,36 @@ def running():
     return False
 
 def playMacro(movements):
-    global last_click_time
     for movement in movements:
         checkpoints = movement['checkpoint']
         for i, checkpoint in enumerate(checkpoints):
-            print(checkpoint)
+            # print(checkpoint)
             if 'mouse_position' in checkpoint:
                 mouse_position = checkpoint['mouse_position']
-                # print('Position: ', mouse_position)
+                print('Position: ', mouse_position)
 
                 mouse_controller.position = (mouse_position)
-                # print(f'Movendo o mouse para : {mouse_position}')
- 
+                print(f'Movendo o mouse para : {mouse_position}')
+
                 if i > 0 and 'time_interval' in checkpoint:
                     time_interval = checkpoint['time_interval']
                     time.sleep(time_interval)
 
-            if 'mouse_clicked' in checkpoint:
-                mouse_controller.press(Button.left)
-                last_click_time = time.time()
+            elif 'keyboard' in checkpoint:
+                keyboard_input = checkpoint['keyboard']
+                print('keyboard_input: ', keyboard_input)
 
-            if 'mouse_released' in checkpoint:
-                if 'time_released' in checkpoint:
-                    time_released = checkpoint['time_released']
-                    time_interval = time_released - last_click_time
+                keyboard_controller.press(keyboard_input)
 
-                    if time_interval > 0:
-                        time.sleep(time_interval)
-                        
-                    mouse_controller.release(Button.left)
+                # if i > 0 and 'time_interval' in checkpoint:
+                #     time_interval = checkpoint['time_interval']
+                #     time.sleep(time_interval)
 
+                keyboard_controller.release(keyboard_controller)
+            
 
 def on_press(key):
-    global isRunning
-    global movements
-    global checkpoints
+    global isRunning, movements, checkpoints
 
     running()
     # print('Tecla pressionada: {0}'.format(
@@ -81,18 +75,22 @@ def on_press(key):
         checkpoints.append(checkpoint)
         movements.clear()
 
+
 def on_release(key):
     running()
-    global movements
+    global movements, last_press
     
     # print('Released {0}'.format(
     #     key
     # ))
 
     if key != keyboard.Key.right:
-        movement = {"keyboard": key}
+        current_time = time.time()
+        time_interval = current_time - last_press
+        movement = {"keyboard": key, "time_interval": time_interval}
         movements.append(movement)
  
+
 def on_move(x,  y):
     global movements, last_time
 
@@ -108,22 +106,19 @@ def on_move(x,  y):
     
 
 def on_click(x, y, button, pressed):
-    global movements, click_time
+    global movements
 
     if running() is False:
         return False
-
     # print('{0} at {1}'.format(
     #     'Pressed' if pressed else 'Released', (x, y)
     # ))
-    current_time = time.time()
     if pressed:
-        click_time = current_time
-        movement = {"mouse_clicked": (x, y), "time_clicked": click_time}
+        movement = {"mouse_clicked": (x, y)}
     else:
-        release_time = current_time
-        movement = {"mouse_released": (x, y), "time_released": release_time}
+        movement = {"mouse_released": (x, y)}
     movements.append(movement)
+
 
 def on_scroll(x, y, dx, dy):
     global movements
@@ -135,6 +130,7 @@ def on_scroll(x, y, dx, dy):
     # ))
     movement = {"mouse_scroll": ((x, y), (dx, dy))}
     movements.append(movement)
+
 
 def start_listeners():
     global isRunning
